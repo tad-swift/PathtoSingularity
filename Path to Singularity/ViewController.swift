9e14f0d1a30a523f143116dde8887cdc98a11601
Tadreik Campbell <tadreik@gmail.com>
2021-03-30 01:43:29 -0400

    Initial Commit

---
+++ Path to Singularity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Path to Singularity
+//
+//  Created by Tadreik Campbell on 3/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
